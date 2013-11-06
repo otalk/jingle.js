30d9cbc99578ba5f05ad75f5f9f9c8a77bacbeef
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-11-06 22:49:08 +0000

    Let me build in debug mode (no uglify + source maps)

--- build.js
+++ build.js
@@ -2,13 +2,18 @@ var browserify = require('browserify');
 var UglifyJS = require('uglify-js');
 var fs = require('fs');
 
-
 var bundle = browserify();
 bundle.add('./index');
-bundle.bundle({standalone: 'Jingle'}, function (err, js) {
+
+var options = { standalone: 'Jingle' }
+if (-1 != process.argv.indexOf('-d')) options.debug = true
+
+bundle.bundle({ standalone: 'Jingle', debug: true }, function (err, js) {
     if (err) {
         console.log(err);
     }
-    var result = UglifyJS.minify(js, {fromString: true}).code;
+    var result = (options.debug) ? js :
+        UglifyJS.minify(js, {fromString: true}).code;
+  
     fs.writeFileSync('jingle.bundle.js', result);
 });
