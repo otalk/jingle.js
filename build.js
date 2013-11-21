b2570d8bc50fdae07dd18e399094fb11f66f940a
Lance Stout <lancestout@gmail.com>
2013-11-21 03:12:23 -0800

    Update dependencies

--- build.js
+++ build.js
@@ -5,8 +5,8 @@ var fs = require('fs');
 var bundle = browserify();
 bundle.add('./index');
 
-var options = { standalone: 'Jingle' }
-if (-1 != process.argv.indexOf('-d')) options.debug = true
+var options = { standalone: 'Jingle' };
+if (-1 != process.argv.indexOf('-d')) options.debug = true;
 
 bundle.bundle({ standalone: 'Jingle', debug: true }, function (err, js) {
     if (err) {
